6cb56bd0cd0f327d0a8c79020728695eef74e047
Isaac <isaac.yeo.linux@gmail.com>
2020-10-12 15:01:54 +0800

    Huge tests and gitignore

---
+++ tests/llist_huge.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "../llist/llist.h"
+
+
+int main(void)
+{
+	struct llist new;
+
+	llist_init(&new, sizeof(int));
+
+	for(int i = 0; i < 1000000; i++)
+		llist_pushback(&new, &(int) {i});
+
+	printf("Memory used: %lu bytes | Constant size %lu + %lu nodes of size (%lu + %lu) each\n", \
+			new.len * (sizeof(struct llist_node) + sizeof(int)) + sizeof(struct llist), \
+			sizeof(struct llist), new.len, sizeof(struct llist_node), sizeof(int));
+
+	llist_deinit(&new);
+}
---
+++ tests/vector_huge.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "../vector/vector.h"
+
+
+int main(void)
+{
+	struct vector new;
+
+	vector_init(&new, sizeof(int));
+
+	for(int i = 0; i < 1000000; i++)
+		vector_pushback(&new, &(int) {i});
+
+	printf("Memory used: %lu bytes | Constant size %lu + %lu elements of size (%lu + %lu) each\n", \
+			sizeof(struct vector) + new.cap * new.dsize + new.cap * sizeof(void*), \
+			sizeof(struct vector), new.cap, new.dsize, sizeof(void*));
+
+	vector_deinit(&new);
+}
