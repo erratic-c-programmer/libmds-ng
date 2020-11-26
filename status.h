4eb892e3eed4e98004d9fb4a015200c7f4b10d2d
Isaac <isaac.yeo.linux@gmail.com>
2020-11-27 01:16:24 +0800

    'Namespaces' for enum

--- status.h
+++ status.h
@@ -10,15 +10,15 @@
  * OOB - Out of bounds
  * NULLPTR - NULL pointer given
  */
-enum status {
-	OK = 0,
-	ALLOC_FAIL,
-	OOB,
-	NULLPTR,
+enum lmds_status {
+	LMDS_OK = 0,
+	LMDS_ALLOC_FAIL,
+	LMDS_OOB,
+	LMDS_NULLPTR,
 };
 
 /* Related error-checking macros */
-#define RETIFNOK(val) if (val != OK) return val;
-#define NULLCHK(in) if (in == NULL) return NULLPTR;
+#define RETIFNOK(val) if (val != LMDS_OK) return val;
+#define NULLCHK(in) if (in == NULL) return LMDS_NULLPTR;
 
 #endif
